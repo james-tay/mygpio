528bc6c02b2b6fabb720e8f4dffceae883bae624
James Tay <james@2longbeans.net>
2020-11-06 18:01:32 -0500

    Added "ota" command which uses http to pull new firmware. Renamed "restart" command to "reload".

--- mygpio.ino
+++ mygpio.ino
@@ -2,6 +2,7 @@
    Build/Upload
 
      % arduino-cli lib install PubSubClient
+     % arduino-cli lib install ArduinoOTA
 
      % arduino-cli compile -b esp32:esp32:esp32 .
      % arduino-cli compile -b esp8266:esp8266:nodemcuv2 .
@@ -76,6 +77,7 @@
        with spaces.
      - All filenames must begin with '/'.
      - LCD row/columns should be configurable.
+     - REST blocks during firmware OTA upgrades.
 
    Threads
 
@@ -180,6 +182,10 @@
      - PubSubClient reference
        https://pubsubclient.knolleary.net/
 
+     - ArduinoOTA reference
+       https://github.com/jandrassy/ArduinoOTA
+       https://github.com/espressif/arduino-esp32/tree/master/libraries/Update/examples/AWS_S3_OTA_Update
+
    Bugs
 
      - tons of buffer overrun opportunities due to static buffers with not
@@ -196,6 +202,7 @@
 
 #if defined ARDUINO_ESP8266_NODEMCU || ARDUINO_ESP32_DEV
   #include <FS.h>
+  #include <Update.h>
   #include <WiFiUdp.h>
   #include <PubSubClient.h>
 
@@ -325,6 +332,7 @@
 #define BLINK_FREQ 5000         // blink to indicate we're alive (ms)
 #define MAX_TOKENS 10
 #define BUF_SIZE 80
+#define BUF_MEDIUM 256
 
 #if defined ARDUINO_ESP8266_NODEMCU || ARDUINO_ESP32_DEV
   #define REPLY_SIZE 1024
@@ -1294,6 +1302,160 @@ void f_handleWebMetrics ()                      // for uri "/metrics"
   Webs.send (200, "text/plain", G_reply_buf) ;
 }
 
+void f_ota (char *url)
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    strcat (G_reply_buf, "WARNING: WiFi not connected.\r\n") ;
+    return ;
+  }
+
+  /* before we connected to the webserver, we need to parse our URL */
+
+  #define READ_TIMEOUT 5
+  #define PROTO "http://"
+  #define CONTENT_TYPE "application/octet-stream"
+
+  char line[BUF_SIZE] ;
+  if (strncmp (PROTO, url, strlen(PROTO)) != 0)
+  {
+    sprintf (line, "WARNING: url does not start with '%s'.\r\n", PROTO) ;
+    strcat (G_reply_buf, line) ;
+    return ;
+  }
+  char *host = url + strlen(PROTO) ;
+  char *uri = strstr (host, "/") ;
+  if (uri != NULL)
+  {
+    int offset = uri - host ;
+    host[offset] = 0 ;
+    uri++ ;
+  }
+  if (uri == NULL)
+    uri = "" ;
+  if (strlen(host) < 1)
+  {
+    strcat (G_reply_buf, "WARNING: Invalid URL specified.\r\n") ;
+    return ;
+  }
+
+  /* Now try make an HTTP GET */
+
+  WiFiClient client ;
+  if (client.connect (host, 80) == false)
+  {
+    sprintf (line, "WARNING: Cannot connect to %s.\r\n", host) ;
+    strcat (G_reply_buf, line) ;
+    return ;
+  }
+
+  char request[BUF_MEDIUM] ;
+  sprintf (request, "GET /%s HTTP/1.1\r\n"
+                    "Host: %s\r\n"
+                    "Cache-Control: no-cache\r\n"
+                    "Connection: close\r\n\r\n",
+                    uri, host) ;
+  client.print (request) ;
+
+  /* read (and verify) HTTP headers from web server */
+
+  long content_length = 0 ;
+  char *content_type=NULL ;
+  unsigned long last_act = millis () ;
+
+  while (client.available () == 0)
+  {
+    if (millis() > last_act + (READ_TIMEOUT * 1000))
+    {
+      strcat (G_reply_buf, "WARNING: No response from web server.\r\n") ;
+      client.stop () ;
+      return ;
+    }
+    delay (10) ;
+  }
+  while (client.available () > 0)
+  {
+    String s = client.readStringUntil ('\n') ;
+    s.trim () ;
+    if (s.length() == 0)                // end of HTTP headers, ie blank line
+      break ;
+
+    if ((s.startsWith("HTTP/1.1")) && (s.indexOf("200") < 0))
+    {
+      sprintf (line, "WARNING: Received %s.\r\n", s.c_str()) ;
+      strcat (G_reply_buf, line) ;
+      client.stop () ;
+      return ;
+    }
+    if (s.startsWith("Content-Length: "))
+    {
+      char *p = strstr (s.c_str(), " ") ;
+      content_length = atoi (p+1) ;
+    }
+    if (s.startsWith("Content-Type: "))
+    {
+      char *p = strstr (s.c_str(), " ") ;
+      content_type = p+1 ;
+      if (strcmp (content_type, CONTENT_TYPE) != 0)
+      {
+        sprintf (line, "WARNING: Wrong Content-Type '%s'\r\n", content_type) ;
+        strcat (G_reply_buf, line) ;
+        client.stop () ;
+        return ;
+      }
+    }
+  }
+
+  if ((content_length == 0) || (content_type == NULL) ||
+      (strlen(content_type) < 1))
+  {
+    strcat (G_reply_buf, "WARNING: Invalid HTTP response.\r\n") ;
+    client.stop () ;
+    return ;
+  }
+
+  if (Update.begin (content_length) == false)
+  {
+    sprintf (line, "WARNING: Insufficient space (%d bytes needed).\r\n",
+             content_length) ;
+    strcat (G_reply_buf, line) ;
+    client.stop () ;
+    return ;
+  }
+
+  /* Print message to the serial console, just in case an operator is there */
+
+  sprintf (request, "NOTICE: Firmware download %s -> %s (%d bytes)",
+           url, content_type, content_length) ;
+  Serial.println (request) ;
+
+  /* Now we perform the actual firmware upgrade, no turning back ! */
+
+  unsigned long tv_start = millis () ;
+  unsigned long amt = Update.writeStream (client) ;
+  if (amt != content_length)
+  {
+    sprintf (line, "WARNING: Only wrote %d out of %d bytes, aborting.\r\n",
+             amt, content_length) ;
+    strcat (G_reply_buf, line) ;
+    client.stop () ;
+    return ;
+  }
+  unsigned long tv_end = millis () ;
+  if ((Update.isFinished()) && (Update.end()))
+  {
+    sprintf (line, "NOTICE: Firmware updated in %d secs, please reboot.\r\n",
+             (tv_end - tv_start) / 1000) ;
+    strcat (G_reply_buf, line) ;
+  }
+  else
+  {
+    sprintf (line, "WARNING: Firmware update failed error %d.\r\n",
+             Update.getError()) ;
+  }
+  client.stop () ;
+}
+
 void f_cron ()
 {
   if ((WiFi.status() != WL_CONNECTED) &&
@@ -1993,7 +2155,8 @@ void f_action (char **tokens)
               "fs rm <filename>\r\n"
               "fs rename <old> <new>\r\n"
               "fs write <filename> <content>\r\n"
-              "restart\r\n"
+              "ota <url>\r\n"
+              "reload\r\n"
               "wifi connect\r\n"
               "wifi scan\r\n"
               "wifi status\r\n"
@@ -2121,13 +2284,19 @@ void f_action (char **tokens)
     f_wifi (tokens) ;
   }
   else
-  if (strcmp(tokens[0], "restart") == 0)
+  if (strcmp(tokens[0], "reload") == 0)
   {
-    Serial.println ("Restarting.") ;
+    Serial.println ("Rebooting.") ;
     delay (1000) ;
     ESP.restart () ;
   }
+  else
+  if ((strcmp(tokens[0], "ota") == 0) && (tokens[1] != NULL))
+  {
+    f_ota (tokens[1]) ;
+  }
   #endif
+
   #ifdef ARDUINO_ESP32_DEV
   else
   if ((strcmp(tokens[0], "adxl335") == 0) && (tokens[1] != NULL) &&
