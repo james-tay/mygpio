316c4649cabf5885ee8f9726adcf14d0f3433905
James Tay <james@2longbeans.net>
2025-12-15 23:12:58 -0500

    f_spi_validate_msg() now validates messages. added the "<MaxIdleSecs>" param to ft_spi().

--- ft_spi.ino
+++ ft_spi.ino
@@ -20,7 +20,7 @@
      SCK        18      14
      CS/SS      5       15
 
-   DATA TRANSFER
+   DATA TRANSFER / PROTOCOL
 
    Note that data transfers on SPI always involve simultaneous reads after
    writes. Specifically, the SPI API functions,
@@ -36,15 +36,33 @@
    format is,
 
      from client:
-       <2-byte length><payload...><0x00, 0x00>
+       <1-byte opcode><2-byte length><payload...><0x00, 0x00>
 
      from server:
-       <2-byte length><payload...><0x00, 0x00>
+       <1-byte opcode><2-byte length><payload...><0x00, 0x00>
 
    The 2-byte length field is in big endian and informs the receiver to expect
-   between 1 and 65535 bytes of payload. At the end of this payload is exactly
+   between 1 and 65535 bytes of payload. In reality, the maximum payload is
+   capped at "SPI_MSG_BUFSIZE - 5". At the end of this payload is exactly
    2x bytes of 0x00. This is a sanity check which allows both parties to
    discard unintentional/errornous traffic (eg, someone telnets by mistake).
+
+   Thus upon connecting to the server, the client is expected to send a message
+   which is at least 6 bytes (ie, 1 + 2 + 1 + 2). This thread delivers the
+   payload to the SPI device while retrieving response bytes at the same time.
+   Depending on the client's opcode, this thread may immediately send the reply
+   to the client, again this is a miminum of 6 bytes.
+
+   OPCODES
+
+   All messages to/from a client must begin with an opcode, which identifies
+   what the message is about. The following opcode IDs are implemented.
+
+      1 - client sending echo request
+      2 - server sending echo response
+      3 - client sending payload, do not send response
+      4 - client sending payload, send response immediately
+      5 - server sending response
 
    IMPLEMENTATION
 
@@ -76,6 +94,12 @@
      spi->endTransaction() ;
      digitalWrite(CS_PIN, HIGH) ;
 
+   In the event that the client is connected to this thread, but suddenly
+   crashes, then this thread keeps the client's socket descriptor open
+   indefinitely. After the client restarts, it will be unable to connect to
+   this thread. For this reason, this thread monitors client idle time and
+   will disconnect a connected client if this idle time is exceeded.
+
    USAGE
 
    This thread expects the following parameters,
@@ -85,15 +109,63 @@
      - MISO pin
      - SCK pin
      - CS pin
+     - Client max allowed idle time (secs)
+
+   To send test message which includes 2x of SPI data to this thread,
+
+     $ echo -ne "\x01\x00\x02\xbe\xef\x00\x00" >/dev/tcp/example.com/9000
 */
 
 #include <SPI.h>
 
-#define SPI_MSG_BUFSIZE 1500    // maximum size of a message to/from client
+/*
+   maximum size of a message to/from client. This value was chosen so that
+   a complete message fits within a single TCP packet (ie, IP + TCP + options)
+   so as to avoid "fragmentation".
+*/
+
+#define SPI_MSG_BUFSIZE 1440
+
+/*
+   This function is called from ft_spi() just after it has performed a recv()
+   with MSG_PEEK on "sd". Thus, "msg" contains a preview of "len" bytes. The
+   incoming message may or may not be valid. A message is valid if,
+     a) first byte is a valid opcode
+     b) next 2x bytes is a valid 16-bit length
+     c) the last 2x bytes are 0x00,0x00
+   If valid, this function returns the length of the message. If invalid, we
+   return -1.
+*/
+
+int f_spi_validate_msg (S_thread_entry *p, int sd, unsigned char *msg, int len)
+{
+  unsigned char opcode ;
+  unsigned short msg_size ;
+
+  if (len < 6)                                  // "msg" is impossibly small
+    return (-1) ;
+
+  opcode = msg[0] ;
+  memcpy (&msg_size, msg+1, sizeof(msg_size)) ;
+  msg_size = ntohs(msg_size) ;
+
+  /* now we know "msg_size", see if "msg" is the expected "len" */
+
+  int expected = 1 + 2 + msg_size + 2 ;
+  if (len < expected)
+    return (-1) ;                               // "msg" is too short
+
+  /* make sure "msg" ends with \x00, \x00 */
+
+  if ((msg[expected-1] != 0) || (msg[expected-2] != 0))
+    return (-1) ;                               // missing \x00 \x00 at the end
+  else
+    return (expected) ;
+}
 
 void ft_spi (S_thread_entry *p)
 {
-  if (p->num_args != 5)
+  if (p->num_args != 6)
   {
     strcpy (p->msg, "FATAL! Expecting 5x arguments") ;
     p->state = THREAD_STOPPED ;
@@ -105,6 +177,7 @@ void ft_spi (S_thread_entry *p)
   int miso_pin = atoi (p->in_args[2]) ;
   int sck_pin = atoi (p->in_args[3]) ;
   int cs_pin = atoi (p->in_args[4]) ;
+  int max_idle_secs = atoi (p->in_args[5]) ;
 
   /* try allocate the SPI message buffer from heap */
 
@@ -148,6 +221,7 @@ void ft_spi (S_thread_entry *p)
     free (spi_buf) ;
     return ;
   }
+  sprintf (p->msg, "listening on %d", listen_port) ;
 
   /*
      this is the main loop, wait for network activity, but check "p->state"
@@ -185,20 +259,32 @@ void ft_spi (S_thread_entry *p)
       }
       if (FD_ISSET (client_sd, &rfds))
       {
-        amt = read (client_sd, spi_buf, SPI_MSG_BUFSIZE) ;
+        amt = recv (client_sd, spi_buf, SPI_MSG_BUFSIZE, MSG_PEEK) ;
         if (amt < 1)                                    // tcp client closed
         {
           close (client_sd) ;
           client_sd = 0 ;
           p->results[0].i_value = 0 ;
         }
+        else
+        {
+          amt = f_spi_validate_msg (p, client_sd, spi_buf, amt) ;
+          if (amt < 0)                                  // invalid message
+          {
+            close (client_sd) ;
+            client_sd = 0 ;
+            p->results[0].i_value = 0 ;
+          }
+          else
+          {
+            amt = read (client_sd, spi_buf, amt) ;      // pull exact message
 
 
 
 
-
+          }
+        }
       }
-
     }
   }
 
